d9808221a88353400fea6564ab2d821038d82469
Lacey Pevey <7490308+pevey@users.noreply.github.com>
2023-10-20 15:44:54 -0500

    update cart function names

--- src/routes/cart/+page.server.ts
+++ src/routes/cart/+page.server.ts
@@ -1,19 +1,19 @@
 import type { Actions } from './$types'
-import { addToCart, removeFromCart, updateCart } from '$lib/server/vendure'
+import { addCartLine, removeCartLine, updateCartLine } from '$lib/server/vendure'
 
 export const actions: Actions = {
    add: async ({ request, locals, cookies }) => {
       const data = await request.formData()
       if (!data.get('variantId')) return { success: false }
       const variantId = data.get('variantId') as string
-      const cart = await addToCart(locals, cookies, variantId, 1)
+      const cart = await addCartLine(locals, cookies, variantId, 1)
       if (cart) return { success: true, cart }
    },
    remove: async({ request, locals, cookies }) => {
       const data = await request.formData()
       const orderLineId = data.get('orderLineId') as string
       if (!orderLineId) return { success: false }
-      const cart = await removeFromCart(locals, cookies, orderLineId)
+      const cart = await removeCartLine(locals, cookies, orderLineId)
       if (cart) return { success: true, cart }
    },
    update: async({ request, locals, cookies }) => {
@@ -22,10 +22,10 @@ export const actions: Actions = {
       const quantity = parseInt(data.get('quantity') as string)
       if (!orderLineId || !quantity) return { success: false }
       if (quantity === 0) { 
-         const cart = await removeFromCart(locals, cookies, orderLineId)
+         const cart = await removeCartLine(locals, cookies, orderLineId)
          if (cart) return { success: true, cart }
       } else if (quantity > 0 && quantity < 12) {
-         const cart = await updateCart(locals, cookies, orderLineId, quantity)
+         const cart = await updateCartLine(locals, cookies, orderLineId, quantity)
          if (cart) return { success: true, cart }
       }
    }
